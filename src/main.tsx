91e8a9edea060b2b34458ea7918d2fb83a005f21
Omar Baldi <omar.baldi@maersk.com>
2023-08-28 10:31:52 +0200

    Imported maersk global packages into application entrypoint file and adjusted import path for using maersk icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
+import '@maersk-global/fonts/maeu/fonts.css';
+import '@maersk-global/icons';
+import { MdsConfig } from '@maersk-global/mds-config';
+import '@maersk-global/mds-design-tokens/maersk/light/css/design-tokens-px.css';
+import '@maersk-global/mds-foundations/css/foundations.css';
+import { getEnvironmentLabel } from '@maersk-global/shared-js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 const rootElement = document.getElementById('root')!;
+
+MdsConfig.iconsDynamicImportPath =
+  getEnvironmentLabel() === 'localhost' ? '../../' : './';
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
