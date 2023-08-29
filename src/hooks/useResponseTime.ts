8ce61c317d223f2ee357a7d01016fbbee03f4d9a
Omar Baldi <omar.baldi@maersk.com>
2023-08-29 12:50:57 +0200

    Fixed response time custom hook logic bug

--- src/hooks/useResponseTime.ts
+++ src/hooks/useResponseTime.ts
@@ -2,11 +2,23 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+/**
+ * @description
+ * Response Time Monitoring
+ * *-------------------------------------------------
+ * Display the time taken for the API call to complete,
+ * helping users assess the performance of the API.
+ */
 export const useResponseTime = () => {
   const [timeTaken, setTimeTaken] = useState<number>(0);
 
   useEffect(() => {
-    const startTime = performance.now();
+    let startTime: ReturnType<typeof performance.now>;
+
+    axios.interceptors.request.use((config) => {
+      startTime = performance.now();
+      return config;
+    });
 
     axios.interceptors.response.use((res) => {
       const endTime = performance.now();
