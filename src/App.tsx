9c5552a985ebfc23a688c9d9d54b1b142facde24
Omar Baldi <omar.baldi@maersk.com>
2023-08-30 16:22:45 +0200

    UI re-design

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@
 import { McButton } from '@maersk-global/mds-react-wrapper';
 import { McInput } from '@maersk-global/mds-react-wrapper/components-core/mc-input';
 import { McLoadingIndicator } from '@maersk-global/mds-react-wrapper/components-core/mc-loading-indicator';
+import { McNotification } from '@maersk-global/mds-react-wrapper/components-core/mc-notification';
 import { useState } from 'react';
 import './App.css';
+import NestedJsonViewer from './components/NestedJsonViewer';
 import { useQueryParams } from './hooks/useQueryParams';
 import { useResponseTime } from './hooks/useResponseTime';
 import { useStructureEndpoint } from './hooks/useStructureEndpoint';
 
 /**
- * TODO: Code Generation -> offer an option to generate code snippets based on the current API configuration. This can help users integrate API calls into their code.
+ * !IMPORTANT [FEATURE]: implement toggle between plain text and tree view
+ * !IMPORTANT [FEATURE]: implement button url to past endpoint (GET method)
+ * !IMPORTANT [FEATURE]: implement download feature
+ * TODO: Code Generation (openAI) -> offer an option to generate code snippets based on the current API configuration. This can help users integrate API calls into their code.
  * TODO: create tooltip context provider (based on maersk component)
  */
 function App() {
@@ -23,123 +28,159 @@ function App() {
     deleteQueryParam,
   } = useQueryParams();
 
-  const { isLoading, data, sendRequest } = useStructureEndpoint({
+  const { isLoading, data, hasError, sendRequest } = useStructureEndpoint({
     endpoint: urlEndpoint,
     queryParams: [...updatedQueryParams.values()],
   });
 
   const timeTakenForApiToComplete = useResponseTime();
 
-  const updatedEndpointWithParams = urlEndpoint.concat(updatedQueryParamsStringFormat);
-
   return (
     <>
-      <h2>{updatedEndpointWithParams}</h2>
+      <div style={{ display: 'flex', gap: '3rem', textAlign: 'start' }}>
+        <div
+          style={{
+            flex: 1,
+            maxWidth: '600px',
+            position: 'sticky',
+            borderRight: '1px solid #cfcfcf',
+            paddingRight: '2rem',
+          }}
+        >
+          <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+            <McInput
+              label='API endpoint'
+              style={{ flex: 1 }}
+              clearbutton
+              hiddenlabel
+              placeholder='Insert your endpoint'
+              width='100'
+              autocomplete='off'
+              value={urlEndpoint}
+              input={(e) =>
+                setUrlEndpoint(
+                  (e as unknown as React.ChangeEvent<HTMLInputElement>).target.value
+                )
+              }
+            />
 
-      <McInput
-        label='API endpoint'
-        fit='large'
-        clearbutton
-        hiddenlabel
-        placeholder='Insert your endpoint'
-        width='auto'
-        autocomplete='off'
-        value={urlEndpoint}
-        input={(e) =>
-          setUrlEndpoint(
-            (e as unknown as React.ChangeEvent<HTMLInputElement>).target.value
-          )
-        }
-      />
+            <McButton label='Call API' loading={isLoading} click={sendRequest} />
+          </div>
 
-      <div style={{ padding: '1rem 0 0 3rem', textAlign: 'start' }}>
-        <h3>Params</h3>
+          <div style={{ margin: '2rem 0 0 1.5rem', textAlign: 'start' }}>
+            <h3>Params</h3>
 
-        {[...updatedQueryParams].map(([key, { name: paramName, value: paramValue }]) => {
-          return (
-            <div
-              key={key}
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '1rem',
-                marginBottom: '1rem',
-              }}
-            >
-              <McInput
-                autocomplete='off'
-                hiddenlabel
-                label=''
-                placeholder='Insert your param key'
-                width='auto'
-                value={paramName}
-                input={(e) => {
-                  const updatedQueryParamName = (
-                    e as unknown as React.ChangeEvent<HTMLInputElement>
-                  ).target.value;
+            {[...updatedQueryParams].map(
+              ([key, { name: paramName, value: paramValue }]) => {
+                return (
+                  <div
+                    key={key}
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      gap: '1rem',
+                      marginBottom: '1rem',
+                    }}
+                  >
+                    <McInput
+                      autocomplete='off'
+                      hiddenlabel
+                      label=''
+                      placeholder='Insert your param key'
+                      width='auto'
+                      value={paramName}
+                      input={(e) => {
+                        const updatedQueryParamName = (
+                          e as unknown as React.ChangeEvent<HTMLInputElement>
+                        ).target.value;
 
-                  updateQueryParam(key, {
-                    name: updatedQueryParamName,
-                    value: paramValue,
-                  });
-                }}
-                clearbutton
-              />
+                        updateQueryParam(key, {
+                          name: updatedQueryParamName,
+                          value: paramValue,
+                        });
+                      }}
+                      clearbutton
+                    />
 
-              <McInput
-                autocomplete='off'
-                hiddenlabel
-                label=''
-                placeholder='Insert your param value'
-                width='auto'
-                value={paramValue}
-                input={(e) => {
-                  const updatedQueryParamValue = (
-                    e as unknown as React.ChangeEvent<HTMLInputElement>
-                  ).target.value;
+                    <McInput
+                      autocomplete='off'
+                      hiddenlabel
+                      label=''
+                      placeholder='Insert your param value'
+                      width='auto'
+                      value={paramValue}
+                      input={(e) => {
+                        const updatedQueryParamValue = (
+                          e as unknown as React.ChangeEvent<HTMLInputElement>
+                        ).target.value;
 
-                  updateQueryParam(key, {
-                    name: paramName,
-                    value: updatedQueryParamValue,
-                  });
-                }}
-                clearbutton
-              />
+                        updateQueryParam(key, {
+                          name: paramName,
+                          value: updatedQueryParamValue,
+                        });
+                      }}
+                      clearbutton
+                    />
 
+                    <McButton
+                      label='Remove'
+                      hiddenlabel
+                      disabled={updatedQueryParams.size <= 1}
+                      appearance='secondary'
+                      trailingicon='times-circle'
+                      click={() => deleteQueryParam(key)}
+                    />
+                  </div>
+                );
+              }
+            )}
+
+            <div style={{ marginTop: '1.5rem' }}>
               <McButton
-                label='Remove'
-                hiddenlabel
-                disabled={updatedQueryParams.size <= 1}
+                label='Add new param'
                 appearance='secondary'
-                trailingicon='times-circle'
-                click={() => deleteQueryParam(key)}
+                trailingicon='plus'
+                click={addNewQueryParam}
               />
             </div>
-          );
-        })}
+          </div>
+        </div>
 
-        <div style={{ marginTop: '1.5rem' }}>
-          <McButton
-            label='Add new param'
-            appearance='secondary'
-            trailingicon='plus'
-            click={addNewQueryParam}
-          />
+        <div
+          style={{
+            maxWidth: '500px',
+            overflow: 'auto',
+            flex: 1,
+          }}
+        >
+          {isLoading ? (
+            <McLoadingIndicator
+              label='Loading data'
+              variant='bar'
+              fit='small'
+              style={{ width: '100%' }}
+            />
+          ) : data ? (
+            <>
+              <div style={{ marginBottom: '2rem' }}>
+                <McNotification
+                  heading={urlEndpoint.concat(updatedQueryParamsStringFormat)}
+                  body={
+                    timeTakenForApiToComplete <= 0
+                      ? 'Wow, that was super fast!'
+                      : `It took ${timeTakenForApiToComplete} seconds to complete the request`
+                  }
+                  appearance='success'
+                />
+              </div>
+
+              <NestedJsonViewer obj={data as any} />
+            </>
+          ) : hasError ? (
+            <div>Error to handle</div>
+          ) : null}
         </div>
       </div>
-
-      <div style={{ display: 'flex', alignItems: 'flex-start', marginTop: '3rem' }}>
-        {/* //!NOTE: button to add new param should be disabled if there is form error */}
-        <McButton label='Call API' click={sendRequest} />
-      </div>
-
-      <h2>Seconds taken: {timeTakenForApiToComplete}</h2>
-
-      {isLoading ? (
-        <McLoadingIndicator label='loading' fit='large' />
-      ) : data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      ) : null}
     </>
   );
 }
